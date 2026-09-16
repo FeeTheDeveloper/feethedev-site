4444c6ec6bc08e02db1e2b6993317134fd4d2f78
Fee The Developer <fee@feethedeveloper.com>
2026-09-15 22:55:58 -0500

    rebuild homepage flow around full business presence bundles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { BusinessPresenceSection } from '@/components/BusinessPresenceSection';
 import { CtaSection } from '@/components/CtaSection';
 import { DesignOptionsSection } from '@/components/DesignOptionsSection';
 import { Hero } from '@/components/Hero';
+import { PackagesSection } from '@/components/PackagesSection';
+import { PartnershipSection } from '@/components/PartnershipSection';
 import { PortfolioSection } from '@/components/PortfolioSection';
 import { ServicesSection } from '@/components/ServicesSection';
 import { VeteranOwnedVerificationSection } from '@/components/sections/veteran-owned-verification-section';
@@ -11,8 +14,11 @@ export default function Home() {
       <div className="absolute left-[-6rem] top-10 h-48 w-48 rounded-full bg-redglow/20 blur-3xl" />
       <div className="absolute right-[-4rem] top-28 h-56 w-56 rounded-full bg-greenglow/15 blur-3xl" />
       <Hero />
-      <VeteranOwnedVerificationSection />
+      <BusinessPresenceSection />
+      <PartnershipSection />
       <ServicesSection />
+      <PackagesSection />
+      <VeteranOwnedVerificationSection />
       <DesignOptionsSection />
       <PortfolioSection />
       <CtaSection />
